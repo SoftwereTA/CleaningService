939c539ff74e2c4d9749c4d6dbde21ff92da4da8
Tariq_mrmsh <tariqmarmash17@gmail.com>
2023-02-17 21:55:38 +0200

    no message

--- CleaningService/src/main/java/Main.java
+++ CleaningService/src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hola");
-        System.out.println("12");
+        System.out.println("1223");
     }
 }
