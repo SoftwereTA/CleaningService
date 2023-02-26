8aaeb5ff1ca8bdd0b1bbfaeb7a010fcd90fd88ed
amerkobari <s12029312@stu.najah.edu>
2023-02-26 22:51:30 +0200

    no message

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -4,15 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("tariq11");
-        System.out.println("tariq");
-        System.out.println("tariq");
 
-        System.out.println("tariq1234");
-
-        System.out.println("tariq");
-
-        System.out.println("ZZZZZZZZZZ");
-        System.out.println("Opszd");
-        System.out.println("hohoh");
     }
 }
