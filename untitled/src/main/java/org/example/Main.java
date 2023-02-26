288d302e374a10ea25bb645cbb0096df07e37b5b
amerkobari <s12029312@stu.najah.edu>
2023-02-26 22:45:15 +0200

    no message

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -5,6 +5,11 @@ public class Main {
 
         System.out.println("tariq11");
         System.out.println("tariq");
+
         System.out.println("tariq1234");
+
+        System.out.println("tariq");
+
+
     }
 }
