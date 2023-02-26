ebaa73877edddacfe1763ee688472381e1e874ac
Tariq_mrmsh <tariqmarmash17@gmail.com>
2023-02-26 22:01:43 +0200

    no message

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("tariq11");
         System.out.println("tariq");
         System.out.println("Zen control");
+        System.out.println("Zen control22");
     }
 }
