4adcc604991e7e3f61ff83393ad440ce8d809dc9
amerkobari <s12029312@stu.najah.edu>
2023-03-07 23:34:28 +0200

    no message

--- demo1/src/test/java/testing/example/ExitTest.java
+++ demo1/src/test/java/testing/example/ExitTest.java
@@ -15,7 +15,26 @@ import static org.junit.Assert.fail;
 
 public class ExitTest {
 
+    Scene1Controller scene1Controller = new Scene1Controller();
+    @When("I click the exit option")
+    public void i_click_the_exit_option() throws IOException {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        scene1Controller.exit(new ActionEvent());
+    }
 
+    @Then("the application should exit")
+    public void the_application_should_exit() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I press ALT+F4 or X")
+    public void i_press_alt_f4_or_x() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        scene1Controller.confirmExit(new Stage());
+    }
 
 
 }
