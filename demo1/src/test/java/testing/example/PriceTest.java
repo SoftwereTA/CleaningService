0dcfc0cb843e62a6fe3548f01a567d57bd775abc
Tariq_mrmsh <tariqmarmash17@gmail.com>
2023-04-16 15:11:04 +0300

    no message

--- demo1/src/test/java/testing/example/PriceTest.java
+++ demo1/src/test/java/testing/example/PriceTest.java
@@ -1,5 +1,5 @@
 package testing.example;
-
+import javafx.scene.control.Alert;
 import com.example.demo1.Main;
 import com.example.demo1.Scene1Controller;
 import com.example.demo1.Scene2Controller;
@@ -11,6 +11,8 @@ import io.cucumber.java.en.When;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -19,32 +21,50 @@ import org.testfx.util.WaitForAsyncUtils;
 
 import java.io.IOException;
 
+import static org.junit.Assert.*;
+
 public class PriceTest {
 
     Scene2Controller obj = new Scene2Controller();
 
+//    @FXML
+//    TextField sizetxt;
     @When("i choose 200x100 size")
     public void i_choose_200x100_size() throws IOException {
         // Write code here that turns the phrase above into concrete actions
         //throw new io.cucumber.java.PendingException();
         obj.SizeHandle1(new ActionEvent());
+        TextField sizetxt = new TextField();
+        sizetxt.setText("you chose 200x100");
 
-
+// Assign sizetxt to obj.sizetxt
+        obj.sizetxt = sizetxt;
+        assertNotNull(obj.sizetxt);
+        assertEquals("you chose 200x100",sizetxt.getText());
     }
 
     @When("i choose speed cleaning")
     public void i_choose_speed_cleaning() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        obj.SpeedBH(new ActionEvent());
+        Platform.runLater(() -> {
+            obj.SpeedBH(new ActionEvent());
+            Alert alert = obj.alert;
+            assertEquals("Speed Cleaning", alert.getTitle());
+            assertEquals(null, alert.getHeaderText());
+            //assertEquals("The Price: 65", alert.getContentText());
+        });
     }
 
     @Then("the price will be {int}")
     public void the_price_will_be(Integer int1) throws IOException {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        obj.getprice();
+        Platform.runLater(() -> {
+            Alert alert = obj.alert;
+            assertNotNull(alert);
+            assertEquals("The Price: "+ obj.getprice(), alert.getContentText());
+        });
 
+//        Alert alert = obj.alert;
+//        assertNotNull(alert);
+//        assertEquals("The Price: "+ obj.getprice(), alert.getContentText());
     }
 
     @When("i choose 200x200 size")
@@ -52,13 +72,24 @@ public class PriceTest {
         // Write code here that turns the phrase above into concrete actions
         //throw new io.cucumber.java.PendingException();
         obj.SizeHandle2(new ActionEvent());
+        TextField sizetxt = new TextField();
+        sizetxt.setText("you chose 200x200");
+
+// Assign sizetxt to obj.sizetxt
+        obj.sizetxt = sizetxt;
+        assertNotNull(obj.sizetxt);
+        assertEquals("you chose 200x200",sizetxt.getText());
     }
 
     @When("i choose deep cleaning")
     public void i_choose_deep_cleaning() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        obj.DeepBH(new ActionEvent());
+        Platform.runLater(() -> {
+            obj.SpeedBH(new ActionEvent());
+            Alert alert = obj.alert;
+            assertEquals("Deep Cleaning", alert.getTitle());
+            assertEquals(null, alert.getHeaderText());
+            //assertEquals("The Price: 65", alert.getContentText());
+        });
     }
 
     @When("i choose 300x200 size")
@@ -66,13 +97,24 @@ public class PriceTest {
         // Write code here that turns the phrase above into concrete actions
         //throw new io.cucumber.java.PendingException();
         obj.SizeHandle3(new ActionEvent());
+        TextField sizetxt = new TextField();
+        sizetxt.setText("you chose 300x200");
+
+// Assign sizetxt to obj.sizetxt
+        obj.sizetxt = sizetxt;
+        assertNotNull(obj.sizetxt);
+        assertEquals("you chose 300x200",sizetxt.getText());
+
     }
 
     @When("i choose dry cleaning")
-    public void i_choose_dry_cleaning() {
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
-        obj.DryBH(new ActionEvent());
-
+    public void i_choose_dry_cleaning() throws InterruptedException {
+        Platform.runLater(() -> {
+            obj.SpeedBH(new ActionEvent());
+            Alert alert = obj.alert;
+            assertEquals("Dry Cleaning", alert.getTitle());
+            assertEquals(null, alert.getHeaderText());
+            //assertEquals("The Price: 65", alert.getContentText());
+        });
     }
 }
