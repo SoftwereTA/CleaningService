fc1c1556d89e8d15b5a28579eb4469e7f05665f8
Tariq_mrmsh <tariqmarmash17@gmail.com>
2023-04-16 15:37:58 +0300

    no message

--- demo1/src/main/java/com/example/demo1/Scene2Controller.java
+++ demo1/src/main/java/com/example/demo1/Scene2Controller.java
@@ -3,12 +3,16 @@ package com.example.demo1;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 
 //import javax.awt.Label;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 
 public class Scene2Controller {
@@ -59,8 +63,18 @@ public class Scene2Controller {
 
     }
     public void switchScene1(ActionEvent event) throws IOException {
-       Main.primaryStage.setScene(Main.scene1);
-        LoggedOutMsg();
+        Platform.runLater(() -> {
+            try {
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Scene1.fxml")));
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                //stage.setScene(new Scene(root));
+                stage.setScene(Main.scene1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        //LoggedOutMsg();
     }
 
     public Parent getRoot() {
