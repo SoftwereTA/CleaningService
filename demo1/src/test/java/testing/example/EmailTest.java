29249e3315e8925bcbe6ff5c15a4c76941da3ca6
Tariq_mrmsh <tariqmarmash17@gmail.com>
2023-04-19 16:49:07 +0300

    no message

--- demo1/src/test/java/testing/example/EmailTest.java
+++ demo1/src/test/java/testing/example/EmailTest.java
@@ -1,2 +1,51 @@
-package testing.example;public class EmailTest {
+package testing.example;
+
+import com.example.demo1.WorkersceneCont;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import javafx.scene.control.TextField;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class EmailTest {
+
+    @When("i click on waiting button")
+    public void i_click_on_waiting_button() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        WorkersceneCont obj = new WorkersceneCont();
+        TextField id = obj.getTextID();
+        String ActMsg =obj.getMsg();
+        String ExpMsg = "Your order has been added to the system and is waiting for a worker to accept it.\n Your order ID is: " + id + "\n Thank you for using our service.\n";
+        assertEquals(ExpMsg.startsWith("Your order has been added"),ActMsg.startsWith("Your order has been added"));
+    }
+
+    @Then("send email")
+    public void send_email() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        WorkersceneCont obj = new WorkersceneCont();
+        assertTrue(obj.getsent());
+    }
+
+    @When("i click on in treatment button")
+    public void i_click_on_in_treatment_button() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        WorkersceneCont obj = new WorkersceneCont();
+        String ActMsg =obj.getMsg();
+        String ExpMsg = "Your order has been accepted by a worker and is being treated.\n Your order ID is: ";
+        assertEquals(ExpMsg.startsWith("Your order has been accepted"),ActMsg.startsWith("Your order has been accepted"));
+    }
+
+    @When("i click on complete button")
+    public void i_click_on_complete_button() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        WorkersceneCont obj = new WorkersceneCont();
+        String ActMsg =obj.getMsg();
+        String ExpMsg = "Your order has been completed.\n Your order ID is: " + obj.getTextID() + "\n Thank you for using our service.\n";
+        assertEquals(ExpMsg.startsWith("Your order has been completed"),ActMsg.startsWith("Your order has been completed"));
+    }
 }
